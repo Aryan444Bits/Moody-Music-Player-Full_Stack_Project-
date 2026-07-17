a64c5b12121a38a7b521a19a90c60bdb0359f22b
Aryan444Bits <branwalaryan71@gmail.com>
2026-07-17 11:25:37 +0530

    bug resolved

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -6,6 +6,7 @@ const cors = require("cors")
 const app = express();
 app.use(cors({
     origin: [
+        "http://localhost:5173",
         "http://localhost:3000",
         "https://moody-music-frontend.onrender.com"
     ],
